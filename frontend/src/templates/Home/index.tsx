c4ae31c6f3ba1c6c268577eac37a93198df0932c
analuisamessias <analuisa.mendes1712@gmail.com>
2025-04-02 20:24:35 -0300

    remove: remove wrong include

--- frontend/src/templates/Home/index.tsx
+++ frontend/src/templates/Home/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Home } from '../../templates/Home/Home.tsx';
+import { Home } from '../../templates/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
