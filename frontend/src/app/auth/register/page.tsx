a910dae6fcf587fe85ccc6b9249606563acbab37
analuisamessias <analuisa.mendes1712@gmail.com>
2025-04-24 23:49:46 -0300

    fix: remove "Auth" div from RegisterForm

--- frontend/src/app/auth/register/page.tsx
+++ frontend/src/app/auth/register/page.tsx
@@ -1,10 +1,5 @@
 import { RegisterForm } from '../../../components/RegisterForm/RegisterForm';
-import { Auth } from '../../../templates/Auth/Auth';
 
 export default async function RegisterPage() {
-	return (
-		<Auth>
-			<RegisterForm />
-		</Auth>
-	);
+	return <RegisterForm />;
 }
