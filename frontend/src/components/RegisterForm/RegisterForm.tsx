aaa46fb9b8facc6df4f33f5580d250ee0c9899a8
analuisamessias <analuisa.mendes1712@gmail.com>
2025-04-24 23:51:01 -0300

    feat: create register form

--- frontend/src/components/RegisterForm/RegisterForm.tsx
+++ frontend/src/components/RegisterForm/RegisterForm.tsx
@@ -1,14 +1,35 @@
 'use client';
+
+import React from 'react';
+import {
+	Container,
+	LeftSection,
+	RightSection,
+	Logo,
+	Title,
+	Form,
+	Input,
+	Button,
+	LinkText,
+} from './RegisterForm.styles';
+import { Register } from '../../templates/Register/Register';
+
 export const RegisterForm = () => {
 	return (
-		<div>
-			<h1>Register Form</h1>
-			<form>
-				<input type="text" placeholder="Username" />
-				<input type="password" placeholder="Password" />
-				<button type="submit">Register</button>
-			</form>
-		</div>
+		<Register>
+			<Title>Faça seu cadastro no DCC Hub</Title>
+			<Form>
+				<Input type="text" placeholder="Nome completo" />
+				<Input type="text" placeholder="Curso" />
+				<Input type="text" placeholder="Matrícula" />
+				<Input type="email" placeholder="E-mail" />
+				<Input type="email" placeholder="Confirme seu e-mail" />
+				<Input type="password" placeholder="Senha" />
+				<Input type="password" placeholder="Confirme sua senha" />
+				<Button>CADASTRAR</Button>
+				<LinkText href="/">Já possui uma conta? Faça login</LinkText>
+			</Form>
+		</Register>
 	);
 };
 
