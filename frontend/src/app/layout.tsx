156df9053fefae69926da1b6a251e012c132f7f2
analuisamessias <analuisa.mendes1712@gmail.com>
2025-04-24 23:48:36 -0300

    fix: correct globalstye to use all page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Epilogue } from 'next/font/google';
+import GlobalStyle from '../styles/global';
 
 const epilogue = Epilogue({
 	weight: ['100', '400', '500', '600', '700', '800'],
@@ -18,7 +19,10 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<GlobalStyle />
+				{children}
+			</body>
 		</html>
 	);
 }
