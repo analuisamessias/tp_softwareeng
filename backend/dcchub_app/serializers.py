43bd3b077849670bf9800a43c8ba768654e6ab8e
Náthally <nathallyfernandes@ufmg.br>
2025-04-26 18:16:17 -0300

    Excluindo arquivos da admin_api e comentando algumas partes de código para teste

---
+++ backend/dcchub_app/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import User, Disciplina
+
+'''class AdminUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+class DisciplinaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Disciplina
+        fields = '__all__' '''
--- backend/dcchub_app/views.py
+++ backend/dcchub_app/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, Disciplina
+from .serializers import AdminUserSerializer, UserSerializer, DisciplinaSerializer
 
-# Create your views here.
+"""class AdminUserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.filter(is_staff=True)
+    serializer_class = AdminUserSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.filter(is_staff=False)
+    serializer_class = UserSerializer
+
+class DisciplinaViewSet(viewsets.ModelViewSet):
+    queryset = Disciplina.objects.all()
+    serializer_class = DisciplinaSerializer """
