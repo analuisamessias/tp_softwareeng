498e502ba288fc33de0a2e972e522bd62823f11f
Matheus <matheus20977@gmail.com>
2025-05-01 13:25:47 -0300

    add tests for listing and creating professors

--- backend/dcchub_app/tests.py
+++ backend/dcchub_app/tests.py
@@ -22,9 +22,12 @@ class EndpointsTestCase(TestCase):
             senha='senha123'
         )
 
-        # Criar um professor
         self.professor = Professor.objects.create(
-            nome='Professor Exemplo'
+            nome='Professor Exemplo 1'
+        )
+
+        self.professor2 = Professor.objects.create(
+            nome='Professor Exemplo 2'
         )
         
         # Criar uma disciplina
@@ -82,6 +85,34 @@ class EndpointsTestCase(TestCase):
         }
         
         response = self.client.post('/api/disciplines/', nova_disciplina, format='json')
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_listar_professores(self):
+        """Qualquer usuário pode listar professores"""
+        response = self.client.get('/api/professores/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 2)
+
+    def test_criar_professor_como_superuser(self):
+        """Apenas admin pode criar professor"""
+        self.client.force_authenticate(user=self.superuser)
+        
+        novo_professor = {
+            'nome': 'Professor Novo'
+        }
+        
+        response = self.client.post('/api/professores/', novo_professor, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_criar_professor_como_usuario_comum(self):
+        """Usuário comum não pode criar professor"""
+        self.client.force_authenticate(user=self.user)
+        
+        novo_professor = {
+            'nome': 'Professor Novo'
+        }
+        
+        response = self.client.post('/api/professores/', novo_professor, format='json')
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 # Create your tests here.
