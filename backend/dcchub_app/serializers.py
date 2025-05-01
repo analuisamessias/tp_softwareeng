22b4a21575234bb003299ae9799f3d39d2ed53a4
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2025-05-01 17:29:19 -0300

    fix: incorrect model in User and AdminUser serializers

--- backend/dcchub_app/serializers.py
+++ backend/dcchub_app/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import User, Disciplina, Professor
 from django.conf import settings
+from django.contrib.auth import get_user_model
 
 class AdminUserSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=False)
     
     class Meta:
-        model = settings.AUTH_USER_MODEL
+        model = get_user_model()
         fields = ['id', 'email', 'nome', 'photo', 'is_staff', 'is_superuser', 'password']
         extra_kwargs = {'password': {'write_only': True}}
     
@@ -31,8 +32,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=False)
     
     class Meta:
-
-        model = settings.AUTH_USER_MODEL
+        model = get_user_model()
         fields = ['id', 'email', 'nome', 'photo', 'password']
         extra_kwargs = {'password': {'write_only': True}}
     
--- backend/dcchub_project/urls.py
+++ backend/dcchub_project/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('dcchub_app.urls')),
+    path('api-auth/', include('rest_framework.urls')),
    # path('test/', test_view, name='test'),
 ]
