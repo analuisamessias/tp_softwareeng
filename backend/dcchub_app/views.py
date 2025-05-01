1406838246203e457019b19ffb955e04c8c92114
Matheus <matheus20977@gmail.com>
2025-05-01 15:06:40 -0300

    add filtering and searching capabilities to DisciplinaViewSet

--- backend/dcchub_app/views.py
+++ backend/dcchub_app/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets, permissions
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+import django_filters
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from .models import User, Disciplina, Professor
 from django.http import JsonResponse
@@ -23,9 +26,20 @@ class UserViewSet(viewsets.ModelViewSet):
             return []
         return [IsAuthenticated()]
 
+class DisciplinaFilter(django_filters.FilterSet):
+    sala = django_filters.CharFilter(field_name='sala', lookup_expr='icontains')  # Filtro parcial para 'sala'
+
+    class Meta:
+        model = Disciplina
+        fields = ['dias', 'sala']
+
 class DisciplinaViewSet(viewsets.ModelViewSet):
     queryset = Disciplina.objects.all()
     serializer_class = DisciplinaSerializer
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+
+    filterset_class = DisciplinaFilter
+    search_fields = ['nome', 'professor__nome']
     
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
