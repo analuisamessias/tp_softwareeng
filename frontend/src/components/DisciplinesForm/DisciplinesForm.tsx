37811302c5d9c7b2095f1807c790485549f29a21
analuisamessias <analuisa.mendes1712@gmail.com>
2025-05-02 18:53:02 -0300

    feat: create disciplines form

--- frontend/src/components/DisciplinesForm/DisciplinesForm.tsx
+++ frontend/src/components/DisciplinesForm/DisciplinesForm.tsx
@@ -1,8 +1,52 @@
 'use client';
 
 import React from 'react';
-import { Title, Form, Input } from './DisciplinesForm.styles';
+import {
+	Title,
+	Form,
+	Input,
+	InfoDisciplines,
+	FormSala,
+	FormDias,
+	FormCod,
+} from './DisciplinesForm.styles';
 
-export const DisciplinesForm = () => {
-	return <Title>Cadastro e Edição de Disciplinas</Title>;
+export type Disciplina = {
+	id: number;
+	nome: string;
+	codigo: string;
+	dia: string;
+	inicio: string;
+	fim: string;
+	professor: string;
+	sala: string;
+	turma: string;
+};
+
+type DisciplinesFormProps = {
+	disciplinas: Disciplina[];
+};
+
+export const DisciplinesForm = ({ disciplinas }: DisciplinesFormProps) => {
+	return (
+		<InfoDisciplines>
+			<Title>Cadastro e Edição de Disciplinas</Title>
+			<Form>
+				<Input type="text" placeholder="Nome da Disciplina" />
+			</Form>
+			<FormCod>
+				<Input type="text" placeholder="Código da Disciplina" />
+			</FormCod>
+			<FormDias>
+				<Input type="text" placeholder="Dias - Ex: '2a e 4a'" />
+				<Input type="text" placeholder="Início" />
+				<Input type="text" placeholder="Fim" />
+			</FormDias>
+			<FormSala>
+				<Input type="text" placeholder="Professor" />
+				<Input type="text" placeholder="Turma" />
+				<Input type="text" placeholder="Prédio - Sala" />
+			</FormSala>
+		</InfoDisciplines>
+	);
 };
