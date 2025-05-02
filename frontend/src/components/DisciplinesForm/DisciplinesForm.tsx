b7ed9811c4883b8cc37c598585ec9e9ce85ef1c9
analuisamessias <analuisa.mendes1712@gmail.com>
2025-05-02 17:12:37 -0300

    fix: remove import AddDisciplines

--- frontend/src/components/DisciplinesForm/DisciplinesForm.tsx
+++ frontend/src/components/DisciplinesForm/DisciplinesForm.tsx
@@ -2,12 +2,7 @@
 
 import React from 'react';
 import { Title, Form, Input } from './DisciplinesForm.styles';
-import { AddDiscipline } from '../../templates/AddDiscipline/AddDiscipline';
 
 export const DisciplinesForm = () => {
-	return (
-		<AddDiscipline>
-			<Title>Cadastro e Edição de Disciplinas</Title>
-		</AddDiscipline>
-	);
+	return <Title>Cadastro e Edição de Disciplinas</Title>;
 };
