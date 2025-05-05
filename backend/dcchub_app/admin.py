b0a29072bd8f31c05785a69ba9d1670b427b0f1f
Gabriel Coelho <coelho.gabrielcs@gmail.com>
2025-05-04 22:17:41 -0300

    feat: add User and Professor manager to Admin page

--- backend/dcchub_app/admin.py
+++ backend/dcchub_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Professor, User
 # Register your models here.
+
+admin.site.register(Professor)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
